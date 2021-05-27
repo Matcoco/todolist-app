b8790cdd55d5c97c383a64ede57c0344a81bac1b
matcoco <b_jayson@hotmail.com>
2021-05-27 16:36:53 +0200

    add reducer and context

--- front/src/components/todolist/TodoLi.js
+++ front/src/components/todolist/TodoLi.js
@@ -1,18 +1,31 @@
 
-const TodoLi = ({ todos, handleDeleteTodo, handleToggleTodo }) => {
+import TodoFormView from "./TodoFormView"
+import styled from 'styled-components';
 
+const TodoLi = ({ todos, handleDeleteTodo, handleToggleTodo, handleEditTodo, toggleEdit }) => {
+
+    const DivEdit = styled.div`
+    display: ${!toggleEdit ? 'block' : "none"}
+    `
+    const View = styled.div`
+    display: ${toggleEdit ? 'block' : 'none'}
+    `
     return (
         <ul>
             {
-                todos.map((todo, index) => (
-                    <li key={index} id={todo.id}>
-                        <div className="view">
-                            <input className="toggle" aria-label="toggle" type="checkbox" defaultChecked={todo.completed ? 'check' : ""} onClick={() => handleToggleTodo(todo.id)} />
-                            <label htmlFor="toggle">{todo.title}</label>
-                            <button className="destroy" onClick={() => { handleDeleteTodo(todo.id) }}>X</button>
-                        </div>
+                todos.map((todo, index) => 
+                    (
+                        <li key={index} id={todo.id}>
+                        <View>
+                            <TodoFormView todos={todos} handleDeleteTodo={handleDeleteTodo} handleToggleTodo={handleToggleTodo} handleEditTodo={handleEditTodo} todo={todo} />
+                        </View>
+                        <DivEdit>
+                            <input className="editTitle" aria-label="toggle" type="text" />
+                            <button className="edit">edit</button>
+                        </DivEdit>
                     </li>
-                ))
+                    )
+                )
             }
         </ul>
     );
---
+++ front/src/reducerAndContext/context.jsx
@@ -0,0 +1,1 @@
+export const myContext = React.createContext();
